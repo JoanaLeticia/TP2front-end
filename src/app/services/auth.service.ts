f26068b278dae0169185020814afb8268a449ff4
Joana Letícia <joanaleticia1973@gmail.com>
2025-04-12 07:55:22 -0300

    first commit

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Recupera o token do localStorage
+    const token = localStorage.getItem('token_auth');
+    
+    // Se existir token, adiciona ao header
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+
+    return next.handle(request);
+  }
+}
